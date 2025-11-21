1a9064452cb3a0d5ed6bc665679d2d2fba9328ba
Arpit Tripathi <arpittripathi755@gmail.com>
2025-11-21 17:14:38 +0530

    final changes done

--- products.js
+++ products.js
@@ -1,17 +1,17 @@
-// Add click animation to cart buttons
-document.addEventListener('DOMContentLoaded', function () {
-    const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+// // Add click animation to cart buttons
+// document.addEventListener('DOMContentLoaded', function () {
+//     const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
 
-    addToCartButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            if (!this.classList.contains('clicked')) {
-                this.classList.add('clicked');
+//     addToCartButtons.forEach(button => {
+//         button.addEventListener('click', function () {
+//             if (!this.classList.contains('clicked')) {
+//                 this.classList.add('clicked');
 
-                // Reset button after 2 seconds
-                setTimeout(() => {
-                    this.classList.remove('clicked');
-                }, 2000);
-            }
-        });
-    });
-});
+//                 // Reset button after 2 seconds
+//                 setTimeout(() => {
+//                     this.classList.remove('clicked');
+//                 }, 2000);
+//             }
+//         });
+//     });
+// });
